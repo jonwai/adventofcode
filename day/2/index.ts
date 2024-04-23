c946a9763f89a92eff75a29ae81f284d5538ad60
jonURT <122555996+jonwai@users.noreply.github.com>
2024-04-23 13:04:18 +0100

    Day 2: Part 2 (correct)

--- day/2/index.ts
+++ day/2/index.ts
@@ -15,33 +15,35 @@ async function processFile(filePath: string): Promise<void> {
 
   for await (const line of rl) {
     let skipLine = false;
-    const games: { red: number, blue: number, green: number }[] = [];
+    const games: { red: number; blue: number; green: number }[] = [];
+    const minimums: { red: number; blue: number; green: number } = {
+      red: 0,
+      blue: 0,
+      green: 0,
+    };
 
     const gameNumberMatch = line.match(/^Game (\d+):/);
     const gameNumber = parseInt(gameNumberMatch[1]);
 
-    const gameSections = line.split(';');
-    gameSections.forEach(section => {
+    const gameSections = line.split(";");
+    gameSections.forEach((section) => {
       const game = { red: 0, blue: 0, green: 0 };
       const matches = section.matchAll(/(\d+) (red|blue|green)/g);
       for (const match of matches) {
         if (match[2]) {
           game[match[2]] += parseInt(match[1]);
+          if (game[match[2]] > minimums[match[2]]) {
+            minimums[match[2]] = game[match[2]];
+          }
         }
       }
       games.push(game);
     });
 
-    games.forEach(game => {
-      if (game.red > 12 || game.green > 13 || game.blue > 14) {
-        skipLine = true;
-      }
-    });
-
-    if (!skipLine) {
-      totalSum += gameNumber;
-    }
-}
+    const power = minimums.red * minimums.green * minimums.blue;
+    console.log(power);
+    totalSum += power;
+  }
 
   rl.close();
   console.log("Total Sum:", totalSum);
